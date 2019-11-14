077279251cd4ff0d7a39e72a8022ab9f16230f38
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-14 18:19:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,25 +4,31 @@ class Dog
   attr_accessor :name
 
   @@all = []
+ 
 
   def initialize(name)
     @name = name
+    
     @@all << self
      
-  end
+    end
+   def save
+      @@all << self
+     
+    end
 
   def self.all
-    @@all.each do |dog|
-      puts dog.name
-    end
-      
+    @@all
   end
-
-  def self.clear_all
+    
+   def self.clear_all
     @@all.clear
   end
-   def self.print_all
-     puts @@all
-     binding.pry
-   end
+    def self.print_all
+      @@all.each do |element|
+        puts element.name
+      
+      end
+    end
+     
 end
