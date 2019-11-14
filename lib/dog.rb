9d4b412f02cfce8382b9a88605cffe6665320f9a
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-14 14:43:13 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,25 @@
-require 'pry'
-# Add your code here
-class Dog                                         #creating class of dog
-  @@all = []                                      #creating class variable to store all dog create from instance of dog
-  
- def initialize(name)                              #create initialize to hook or callback data 
-  @name = name                                     #create instance variable which enable local variable to be casted 
-  @@all << self                                    
- end
-  
- def self.all
-   @@all
- end
- 
- def self.print_all
-   
-   @@all.each do |word|
-     
-    puts word
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
       
-    end 
- end
-  
+  end
+
   def self.clear_all
     @@all.clear
   end
+   def self.print_all
+     puts @@all
+   end
 end
